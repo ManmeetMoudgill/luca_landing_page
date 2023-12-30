0db22f7de0ebf4d36a9614d5645772d7bd8c3042
manmeetmoudgill <manmeetmoudgill76@gmail.com>
2023-12-30 15:24:23 +0100

    done with initial settings

---
+++ components/shared/header/navbarListItem/index.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import React from "react";
+
+type NavbarListItemProps = {
+  text: string;
+  link: string;
+};
+
+const NavbarListItem = ({ text, link }: NavbarListItemProps) => {
+  return (
+    <li className="text-white text-lg mx-4  flex flex-col group">
+      <Link href={link}>
+        <span>{text}</span>
+      </Link>
+      <span className="w-[0px] group-hover:w-full transition-all delay-75 ease-in-out min-h-[2px] bg-[#3cd13e]"></span>
+    </li>
+  );
+};
+
+export default NavbarListItem;
