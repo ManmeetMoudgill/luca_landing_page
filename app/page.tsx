200399d19a52ec065636bbf14fea2ca05481c41b
manmeetmoudgill <manmeetmoudgill76@gmail.com>
2024-01-01 08:23:05 +0100

    dev

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import About from "@/components/about";
 import IntroductionContainer from "@/components/introduction";
 import Information from "@/components/information";
+import Reviews from "@/components/reviews";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <IntroductionContainer />
       <About />
       <Information />
+      <Reviews />
     </main>
   );
 }
--- components/introduction/index.tsx
+++ components/introduction/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Reviews from "../shared/reviews";
+import Reviews from "../shared/review";
 
 type Props = {};
 
