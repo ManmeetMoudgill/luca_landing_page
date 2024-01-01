0c5a279019d243b410d8776bebed383ef730141a
manmeetmoudgill <manmeetmoudgill76@gmail.com>
2024-01-01 15:43:16 +0100

    performace chart almost done

--- components/performanceChart/index.tsx
+++ components/performanceChart/index.tsx
@@ -4,23 +4,18 @@ const PerformanceChart = () => {
   return (
     <div className="px-[1.5rem] md:px-[5rem]">
       <div className="py-8 md:py-12 relative ">
-        <div className=" md:mx-auto md:w-[95%] lg:w-[75%] xl:w-[55%] border-2 border-red-900 flex justify-center ">
-          <div
-            style={{
-              width: "min-content",
-            }}
-            className="border-2 relative border-yellow-500  gap-[10px] sm:gap-8  grid  p-6 min-[375px]:p-10   min-[320px]:grid-cols-[120px,120px] min-[320px]:grid-rows-[120px,120px]  min-[375px]:grid-cols-[1fr,1fr] min-[375px]:grid-rows-[1fr,1fr]  md:grid-cols-[16rem,16rem] md:grid-rows-[16rem,16rem] lg:grid-cols-[18rem,18rem] lg:grid-rows-[18rem,18rem]"
-          >
-            <div className=" border-2 border-red-900 min-[375px]:min-h-[10rem] sm:min-h-[15rem] md:min-h-auto   p-1 min-[375px]:p-4 rounded-xl bg-[#EDEDED] shadow-sm">
+        <div className=" md:mx-auto md:w-[95%] lg:w-[75%] xl:w-[55%]  flex justify-center ">
+          <div className=" w-full lg:w-min relative  gap-[10px] sm:gap-8  grid  p-6 min-[375px]:p-10 grid-rows-[1fr,1fr] grid-cols-[1fr,1fr]  lg:grid-cols-[18rem,18rem] lg:grid-rows-[18rem,18rem]">
+            <div className="  min-[375px]:min-h-[10rem] sm:min-h-[15rem] md:min-h-auto   p-1 min-[375px]:p-4 rounded-xl bg-[#EDEDED] shadow-sm">
               asdasdasdsad
             </div>
-            <div className=" border-2 border-red-900 min-[375px]:min-h-[10rem] sm:min-h-[15rem] md:min-h-auto   p-1 min-[375px]:p-4 rounded-xl bg-[#EDEDED] shadow-sm">
+            <div className="  min-[375px]:min-h-[10rem] sm:min-h-[15rem] md:min-h-auto   p-1 min-[375px]:p-4 rounded-xl bg-[#EDEDED] shadow-sm">
               asdasdasdsad
             </div>
-            <div className="border-2 border-red-900 min-[375px]:min-h-[10rem]  sm:min-h-[15rem] md:min-h-auto   p-1 min-[375px]:p-4 rounded-xl bg-[#EDEDED] shadow-sm">
+            <div className=" min-[375px]:min-h-[10rem]  sm:min-h-[15rem] md:min-h-auto   p-1 min-[375px]:p-4 rounded-xl bg-[#EDEDED] shadow-sm">
               asdasdasdsad
             </div>
-            <div className=" border-2 border-red-900 min-[375px]:min-h-[10rem] sm:min-h-[15rem] md:min-h-auto  p-1 min-[375px]:p-4 rounded-xl bg-[#EDEDED] shadow-sm">
+            <div className="  min-[375px]:min-h-[10rem] sm:min-h-[15rem] md:min-h-auto  p-1 min-[375px]:p-4 rounded-xl bg-[#EDEDED] shadow-sm">
               asdasdasdsad
             </div>
             <div className="graph__lines absolute top-0 left-0 w-full h-full">
