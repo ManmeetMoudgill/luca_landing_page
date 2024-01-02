937a6f222b2abb2b2c5989118ed43159462fd796
manmeetmoudgill <manmeetmoudgill76@gmail.com>
2024-01-02 15:20:31 +0100

    optimzied some things

--- components/nextLevel/index.tsx
+++ components/nextLevel/index.tsx
@@ -5,7 +5,7 @@ const index = () => {
   return (
     <div className="text-[#707070] text-xl px-[1.5rem] md:px-[5rem]  justify-center items-center">
       <div className=" justify-center items-center py-8 md:py-12">
-        <div className="bg_img p-8 lg:p-16 flex flex-col justify-center items-center mx-auto max-w-[55rem]">
+        <div className="bg_img p-12 lg:p-28 rounded-2xl flex flex-col justify-center items-center mx-auto max-w-[55rem]">
           <p className="my-2">AND IF YOU'RE ALL CAUGHT-UP…</p>
           <p className="mb-6 text-white text-3xl text-center">
             It's time for you to take your trading to the next level
--- components/performanceChart/index.tsx
+++ components/performanceChart/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const PerformanceChart = () => {
@@ -5,9 +6,17 @@ const PerformanceChart = () => {
     <div className="px-[1.5rem] md:px-[5rem]">
       <div className="py-8 md:py-12 relative ">
         <div className=" md:mx-auto md:w-[95%] lg:w-[75%] xl:w-[55%]  flex justify-center ">
-          <div className=" w-full lg:w-min relative  gap-[10px] sm:gap-8  grid  p-6 min-[375px]:p-10 grid-rows-[1fr,1fr] grid-cols-[1fr,1fr]  lg:grid-cols-[18rem,18rem] lg:grid-rows-[18rem,18rem]">
+          <div className=" w-full lg:w-min relative  gap-[10px] sm:gap-8  grid  pl-6 pb-6  min-[375px]:pl-[2.5rem]   min-[375px]:pb-[2.5rem] grid-rows-[1fr,1fr] grid-cols-[1fr,1fr]  lg:grid-cols-[18rem,18rem] lg:grid-rows-[18rem,18rem]">
             <div className="  min-[375px]:min-h-[10rem] sm:min-h-[15rem] md:min-h-auto   p-1 min-[375px]:p-4 rounded-xl bg-[#EDEDED] shadow-sm"></div>
-            <div className="  min-[375px]:min-h-[10rem] sm:min-h-[15rem] md:min-h-auto   p-1 min-[375px]:p-4 rounded-xl bg-[#EDEDED] shadow-sm"></div>
+            <div className="  min-[375px]:min-h-[10rem] sm:min-h-[15rem] md:min-h-auto  rounded-xl bg-[#EDEDED] shadow-sm">
+              <Image
+                src="/assets/images/traders.avif"
+                width={375}
+                height={375}
+                alt="chart"
+                className="w-full h-full object-cover"
+              />
+            </div>
             <div className=" min-[375px]:min-h-[10rem]  sm:min-h-[15rem] md:min-h-auto   p-1 min-[375px]:p-4 rounded-xl bg-[#EDEDED] shadow-sm"></div>
             <div className="  min-[375px]:min-h-[10rem] sm:min-h-[15rem] md:min-h-auto  p-1 min-[375px]:p-4 rounded-xl bg-[#EDEDED] shadow-sm"></div>
             <div className="graph__lines absolute top-0 left-0 w-full h-full">
@@ -25,7 +34,7 @@ const PerformanceChart = () => {
                   transform="translate(1704.707 7636.414)"
                   fill="#DDDDDD"
                   stroke="#DDDDDD"
-                  stroke-width="2"
+                  strokeWidth="2"
                 ></path>
               </svg>
               <svg
@@ -42,7 +51,7 @@ const PerformanceChart = () => {
                   transform="translate(-7624.293 1704.707) rotate(90)"
                   fill="#DDDDDD"
                   stroke="#DDDDDD"
-                  stroke-width="2"
+                  strokeWidth="2"
                 ></path>
               </svg>
             </div>
